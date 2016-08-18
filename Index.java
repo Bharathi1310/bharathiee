65902f1ce2fc57b5dfeb326cefb8fd6ac2502c88
Bharathi1310 <bharathikalai1310@gmail.com>
2016-08-18 04:41:46 -0700

    Create Index.java

---
+++ Index.java
@@ -0,0 +1,12 @@
+public class Index {
+public static void main(String[] args) {
+int a[]=new int[]{0,1,3,4};
+for(int i=0;i<a.length;i++){
+	if(i==a[i]){
+		System.out.println(a[i]);
+	}
+}
+   
+	}
+}
+				
